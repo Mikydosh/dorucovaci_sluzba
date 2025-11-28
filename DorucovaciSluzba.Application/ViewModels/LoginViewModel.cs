a2862e2f7e0245968ec786b86086ee6f1c794f86
Mikydosh <michal.dolik@email.cz>
2025-11-28 22:20:40 +0100

    ViewModels pro registrační a přihlašovací formulář + Vytvoření LoginViewModel a RegisterViewModel pro formuláře s registrací a přihlašováním

---
+++ DorucovaciSluzba.Application/ViewModels/LoginViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace DorucovaciSluzba.Application.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required]
+        public string? Username { get; set; }
+
+        [Required]
+        public string? Password { get; set; }
+
+        [Required]
+        public bool LoginFailed { get; set; }
+    }
+}
